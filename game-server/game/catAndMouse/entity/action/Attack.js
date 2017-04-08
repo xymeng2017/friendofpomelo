2e9468f5a41f69699c9c86f7e638e7066e6ab85d
lee <steve@lee.com>
2017-04-09 03:32:27 +0800

    first commit

--- game-server/game/catAndMouse/entity/action/Attack.js
+++
@@ -1,8 +0,0 @@
-/* *******************************************
- * Copyright (c) 2016-2017
- * Heisenberg Co.,Ltd. All rights reserved.
- * Project      : treasures
- * File         : Attack.js
- * Created      : 2017-03-07
- * Author       : lee
- * *******************************************/
--- game-server/game/catAndMouse/entity/action/Move.js
+++
@@ -1,8 +0,0 @@
-/* *******************************************
- * Copyright (c) 2016-2017
- * Heisenberg Co.,Ltd. All rights reserved.
- * Project      : treasures
- * File         : Move.js
- * Created      : 2017-03-07
- * Author       : lee
- * *******************************************/
